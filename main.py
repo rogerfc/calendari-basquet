8b7fdd8fff06cd6915415957de6e7cc705654c35
Roger Firpo <roger.firpo@gmail.com>
2024-03-02 06:30:56 +0100

    actualització feb24

--- main.py
+++ main.py
@@ -1,37 +1,68 @@
+#!/usr/bin/env python
+
 import requests
 import pandas as pd
 import datetime
 from ics import Calendar, Event
+import yaml
 
-# Mini Masculí 1er any
-url = 'https://www.basquetcatala.cat/partits/calendari_equip_global/275/62680'
+url_calendari = "https://www.basquetcatala.cat/partits/calendari_equip_global/{id_club}/{id_equip}"
 
-html = requests.get(url).content
-df_list = pd.read_html(html)
-df = df_list[-1]
-df['begin'] = pd.to_datetime(df["Data"] + ' ' + df["Hora"], format='%d/%m/%Y %H:%M').dt.tz_localize('Europe/Andorra')
+def main():
+    equips = get_teams()
+    for equip in equips:
+        print(equip['nom_curt'])
+        df = get_team_schedule(team=equip)
+        df = enrich_schedule(df, team=equip)
+        write_calendar_file(df, team=equip)
 
-print(df)
+def get_teams():
+    with open('equips.yml', 'r') as file:
+        teams = yaml.safe_load(file)
+    return teams.get('equips')
 
-c = Calendar()
-# Index(['Data', 'Hora', 'Equip Local', 'Equip Visitant', 'Categoria',
-#        'Camp de joc', '[+]'],
-#       dtype='object')
+def get_team_schedule(team):
+    url = url_calendari.format(**team)
+    html = requests.get(url).content
+    df_list = pd.read_html(html)
+    df = df_list[-1]    
+    return df
+
+def enrich_schedule(df, team):
+    # Start time in CET
+    df['begin'] = pd.to_datetime(df["Data"] + ' ' + df["Hora"], format='%d/%m/%Y %H:%M').dt.tz_localize('Europe/Andorra')
+    # Event name
+    df['event_name'] = [
+        set_event_name(team, local=x, visitor=y)
+        for x,y in zip(df['Equip Local'], df['Equip Visitant'])
+    ]
+    return df
+
+def set_event_name(team, local, visitor):
+    if team['nom'] in local:
+        return '🏀 {casb} vs. {visitor}'.format(casb=team['nom_curt'],visitor=visitor) 
+    elif team['nom'] in visitor:
+        return '🏀 {casb} @ {local}'.format(casb=team['nom_curt'],local=local) 
+    else:
+        print('error')
+        return '🏀 {local} vs. {visitor}'.format(local=local,visitor=visitor) 
+
+def write_calendar_file(df, team):
+    c = Calendar()
+    filename = '{filename}.ics'.format(filename=team['nom_curt'].replace(' ', '-').lower())
+    for index, row in df.iterrows():
+        e = Event()
+        e.name = row['event_name']
+        e.begin = row["begin"]
+        e.duration = datetime.timedelta(minutes=90)
+        # e.description = ""
+        e.location = row["Camp de joc"]
+        # e.url = ""
+        c.events.add(e)
+    # print(c.events)
+    with open(filename, 'w') as f:
+        f.writelines(c.serialize_iter())
 
 
-for index, row in df.iterrows():
-    e = Event()
-    e.name = f'{row["Equip Local"]} vs. {row["Equip Visitant"]}'
-    e.begin = row["begin"]
-    e.duration = datetime.timedelta(minutes=90)
-#     e.description = ""
-    e.location = row["Camp de joc"]
-#     e.url = ""
-    c.events.add(e)
-
-print(c.events)
-
-with open('casb_mini.ics', 'w') as f:
-    f.writelines(c.serialize_iter())
-
-# df.to_csv('my data.csv')
+if __name__ == "__main__":
+   main()
